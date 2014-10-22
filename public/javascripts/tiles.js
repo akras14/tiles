0bef435720e2f82b07eceb7eefb9aa34a0e74f81
Alex Kras <alex.kras@citrix.com>
2014-10-21 21:38:57 -0700

    Added clear button

--- public/javascripts/tiles.js
+++ public/javascripts/tiles.js
@@ -8,29 +8,82 @@
         var red = getRandomLight();
         var green = getRandomLight();
         var blue = getRandomLight();
-        var randomColor =  'rgb(' + red + ', ' + green + ', ' + blue + ')';
+        return {
+            r: red,
+            g: green,
+            b: blue
+        }
+    }
+
+    function getColorString(colors){
+        var randomColor =  'rgb(' + colors.r + ', ' + colors.g + ', ' + colors.b + ')';
         return randomColor;
     }
 
-    function addClickListenersToTiles() {
-        $('.tile').each(function(){
-            var tile = $(this);
-            var backgroundChanged = false;
-            tile.on('click', function(){
-                if(backgroundChanged){
-                    tile.css('background', '');
-                    backgroundChanged = false;
-                } else {
-                    var randomColor = getRandomColor();
-                    tile.css('background', randomColor);
-                    backgroundChanged = true;
-                }
+    var tiles = (function(){
+
+        var tiles = {};
+        var WHITE_COLOR = {
+            r: 255,
+            g: 255,
+            b: 255
+        };
+
+        function updateBackgroundFor($tile, index){
+            return $tile.css('background', getColorString(tiles[index].color));
+        }
+
+        function initTiles(){
+            $('.tile').each(function(index){
+                tiles[index] = {
+                    backgroundChanged: false,
+                    color: WHITE_COLOR
+                };
             });
-        });
-    }
+        }
+
+        function addTilesClickListener() {
+            $('.tile').each(function(index){
+                var $tile = $(this);
+                $tile.on('click', function(){
+                    if(tiles[index].backgroundChanged){
+                        tiles[index].color = WHITE_COLOR;
+                        tiles[index].backgroundChanged = false;
+                    } else {
+                        tiles[index].color = getRandomColor();
+                        tiles[index].backgroundChanged = true;
+                    }
+                    updateBackgroundFor($tile, index);
+                });
+            });
+        }
+
+        function addClearClickListener() {
+            $('.clear').on('click', function(){
+                //1. Update tiles object
+                $.each(tiles, function(index){
+                    tiles[index].backgroundChanged = false;
+                    tiles[index].color = WHITE_COLOR;
+                });
+
+                //2. Update view
+                $('.tile').each(function(index){
+                    var $tile = $(this);
+                    updateBackgroundFor($tile, index);
+                });
+            });
+        }
+        return {
+            init: function(){
+                initTiles();
+                addTilesClickListener();
+                addClearClickListener();
+            }
+        }
+    })();
 
     $(document).ready(function(){
-        addClickListenersToTiles();
+        tiles.init();
     });
 
 })(jQuery);
